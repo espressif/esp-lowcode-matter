3398c42276f4365a56edcecc69e843c92e802eaa
Strandkiefer <131675660+Strandkiefer@users.noreply.github.com>
2025-09-16 14:23:53 +0000

    WindowsCovering eingeführt

--- components/relay/relay_driver.c
+++ components/relay/relay_driver.c
@@ -10,6 +10,7 @@ void relay_driver_init(int gpio_num)
     ulp_lp_core_gpio_output_enable(gpio_num);
     ulp_lp_core_gpio_input_disable(gpio_num);
     ulp_lp_core_gpio_set_output_mode(gpio_num, RTCIO_LL_OUTPUT_NORMAL);
+ 
 }
 
 void relay_driver_set_power(int gpio_num, bool power)
